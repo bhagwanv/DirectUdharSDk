7c21ff0e5a1f40e6eb6b3d7b600d8f35025d58af
Mukesh <mukesh.patel@shopkirana.com>
2023-08-10 19:52:30 +0530

    ui Done

---
+++ directUdhar/src/main/java/com/sk/directudhar/ui/applyloan/BusinessDetailsFragment.kt
@@ -0,0 +1,50 @@
+package com.sk.directudhar.ui.applyloan
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.sk.directudhar.databinding.FragmentAadhaarCardBinding
+import com.sk.directudhar.databinding.FragmentApplyLoanBinding
+import com.sk.directudhar.databinding.FragmentBusinessDetailsBinding
+import com.sk.directudhar.ui.applyloan.ApplyLoanFactory
+import com.sk.directudhar.ui.applyloan.ApplyLoanViewModel
+import com.sk.directudhar.ui.mainhome.MainActivitySDk
+import javax.inject.Inject
+
+class BusinessDetailsFragment : Fragment() {
+    lateinit var activitySDk: MainActivitySDk
+
+    private lateinit var mBinding: FragmentBusinessDetailsBinding
+
+    lateinit var applyLoanViewModel: ApplyLoanViewModel
+
+    lateinit var proceedBottomDialog: BottomSheetDialog
+
+    @Inject
+    lateinit var applyLoanFactory: ApplyLoanFactory
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        activitySDk = context as MainActivitySDk
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        if (mBinding == null) {
+            mBinding = FragmentBusinessDetailsBinding.inflate(inflater, container, false)
+            initView()
+        }
+        return mBinding!!.root
+    }
+
+    private fun initView() {
+
+    }
+}
