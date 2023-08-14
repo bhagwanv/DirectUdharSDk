151097d625cd233a0b945a45ceaa53fc7011466b
AtulSinghSk <atul@shopkirana.com>
2023-08-14 16:31:08 +0530

    Bussiness

---
+++ directUdhar/src/main/java/com/sk/directudhar/ui/applyloan/BusinessDetailsVerifyElectricityBillRequestModel.kt
@@ -0,0 +1,5 @@
+package com.sk.directudhar.ui.applyloan
+
+data class BusinessDetailsVerifyElectricityBill(  val LeadMasterId: Int,
+                                                  val CustomerId: String,
+                                                  val ServiceProvider: String)
---
+++ directUdhar/src/main/java/com/sk/directudhar/ui/applyloan/BusinessDetailsVerifyElectricityBillResponseModel.kt
@@ -0,0 +1,3 @@
+package com.sk.directudhar.ui.applyloan
+
+data class businessDetailsVerifyElectricityBillResponseModel()
