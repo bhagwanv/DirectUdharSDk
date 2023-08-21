5f96c81e4e99e9ef351a887b5d3816047d726bcb
goleyogesh <yogesh@shopkirana.com>
2023-08-21 18:14:26 +0530

    aadhaar extra spaces

--- directUdhar/src/main/java/com/sk/directudhar/ui/adharcard/aadhaarCardOtp/AadhaarOtpViewModel.kt
+++ directUdhar/src/main/java/com/sk/directudhar/ui/adharcard/aadhaarCardOtp/AadhaarOtpViewModel.kt
@@ -8,9 +8,11 @@ import com.google.gson.JsonObject
 import com.sk.directudhar.MyApplication
 import com.sk.directudhar.data.NetworkResult
 import com.sk.directudhar.ui.adharcard.AadhaarUpdateResponseModel
+import com.sk.directudhar.ui.adharcard.UpdateAadhaarInfoRequestModel
 import com.sk.directudhar.ui.adharcard.aadhaarManullyUpload.AadhaarManuallyUploadResponseModel
 import com.sk.directudhar.ui.mainhome.InitiateAccountModel
 import com.sk.directudhar.utils.Network
+import com.sk.directudhar.utils.SingleLiveEvent
 import com.sk.directudhar.utils.Utils.Companion.AADHAAR_OTP_VALIDATE_SUCCESSFULLY
 import com.sk.directudhar.utils.Utils.Companion.toast
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -22,15 +24,17 @@ import javax.inject.Inject
 class AadhaarOtpViewModel @Inject constructor(private val repository: AadhaarOtpRepository) :
     ViewModel() {
 
-    private val aadhaarResultResult = MutableLiveData<String>()
-    fun getAadhaarResult(): LiveData<String> = aadhaarResultResult
+    private val aadhaarResultResult = SingleLiveEvent<String>()
+    fun getAadhaarResult(): SingleLiveEvent<String> = aadhaarResultResult
 
-    private var _postDataResponse = MutableLiveData<NetworkResult<InitiateAccountModel>>()
-    val postResponse: LiveData<NetworkResult<InitiateAccountModel>> = _postDataResponse
+    private var _postDataResponse = SingleLiveEvent<NetworkResult<InitiateAccountModel>>()
+    val postResponse: SingleLiveEvent<NetworkResult<InitiateAccountModel>> = _postDataResponse
 
-    private var putUploadImageResponse = MutableLiveData<NetworkResult<AadhaarManuallyUploadResponseModel>>()
-    val getUploadImageResponse: LiveData<NetworkResult<AadhaarManuallyUploadResponseModel>> = putUploadImageResponse
+    private var putUploadImageResponse = SingleLiveEvent<NetworkResult<AadhaarManuallyUploadResponseModel>>()
+    val getUploadImageResponse: SingleLiveEvent<NetworkResult<AadhaarManuallyUploadResponseModel>> = putUploadImageResponse
 
+    private var _sendOtpDataResponse = SingleLiveEvent<NetworkResult<AadhaarUpdateResponseModel>>()
+    val putOtpResponse: SingleLiveEvent<NetworkResult<AadhaarUpdateResponseModel>> = _sendOtpDataResponse
     fun validateOtp(otp: String) {
         if (otp.isNullOrEmpty()) {
             aadhaarResultResult.value = "Please Enter OTP"
@@ -58,6 +62,18 @@ class AadhaarOtpViewModel @Inject constructor(private val repository: AadhaarOtp
             viewModelScope.launch {
                 repository.uploadAadhaarImage(body,leadMasterId).collect() {
                     putUploadImageResponse.postValue(it)
+                }
+            }
+        } else {
+            (MyApplication.context)!!.toast("No internet connectivity")
+        }
+    }
+
+    fun updateAadhaarInfo(updateAadhaarInfoRequestModel: UpdateAadhaarInfoRequestModel) {
+        if (Network.checkConnectivity(MyApplication.context!!)) {
+            viewModelScope.launch {
+                repository.updateAadhaarInfo(updateAadhaarInfoRequestModel).collect() {
+                    _sendOtpDataResponse.postValue(it)
                 }
             }
         } else {
